5a5e2cc6610e9f6e58fd2c90d4b314fae3601856
popo <172829492+rlopez-2023066@users.noreply.github.com>
2025-06-13 16:43:27 -0600

    Create Login.jsx

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import imageLogo from '../../assets/logoAgroMarket.png';
+import bgImage from '../../assets/bg-login.png';
+
+export const Login = () => {
+    return (
+        <section className="relative min-h-screen overflow-hidden">
+            {/* Imagen de fondo desenfocada */}
+            <div
+                className=" absolute inset-0 bg-cover bg-center filter blur-sm scale-110"
+                style={{ backgroundImage: `url(${bgImage})` }}
+            ></div>
+
+            {/* Contenido encima de la imagen */}
+            <div className="relative z-10 flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen">
+                
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="bg-stone-50 rounded-xl w-40 h-40 mr-2 p-4" src={imageLogo} alt="logo" />
+                </a>
+
+                <div className="w-full rounded-xl  sm:max-w-md xl:p-0 bg-stone-50 ">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+                            Iniciar Sesión
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            </div>
+
+                            <button type="submit" className="bg-green-700 hover:bg-emerald-400 w-full text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer">Iniciar sesión</button>
+                            <p className="text-center text-sm font-light text-gray-500 dark:text-gray-400">
+                                ¿No tienes cuenta? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Regístrate</a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
