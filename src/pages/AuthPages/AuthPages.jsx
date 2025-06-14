6bbec9d579a33d96bff4acd3ea5f022cc06c06a3
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-14 00:29:09 -0600

    Update AuthPages.jsx

--- src/pages/AuthPages/AuthPages.jsx
+++ src/pages/AuthPages/AuthPages.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Login } from '../../components/Auth/Login';
 import { Register } from '../../components/Auth/Register';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import Home from '../Home/Home';
 
 const AuthContainer = styled.div`
  
@@ -21,7 +24,7 @@ export const AuthPages = () => {
 
   return (
     <AuthContainer>
-      <Register />
+      <Login />
     </AuthContainer>
   );
 };
