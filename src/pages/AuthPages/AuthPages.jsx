c2962b3dd5088278e0fb7dc1cbd662a11827551b
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-14 19:39:52 -0600

    Update AuthPages.jsx

--- src/pages/AuthPages/AuthPages.jsx
+++ src/pages/AuthPages/AuthPages.jsx
@@ -2,21 +2,13 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Login } from '../../components/Auth/Login';
 import { Register } from '../../components/Auth/Register';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/Footer/Footer';
-import Home from '../Home/Home';
 
 const AuthContainer = styled.div`
- 
   
 `;
 
-
-
 export const AuthPages = () => {
   const [isLoginActive, setIsLoginActive] = useState(true);
-
- 
 
   const switchAuthHandler = () => {
     setIsLoginActive(prevState => !prevState);
@@ -24,7 +16,29 @@ export const AuthPages = () => {
 
   return (
     <AuthContainer>
-      <Login />
+      {isLoginActive ? (
+        <Login onSwitch={switchAuthHandler} />
+      ) : (
+        <Register onSwitch={switchAuthHandler} />
+      )}
+    </AuthContainer>
+  );
+};
+
+export const AuthPagesRegister = () => {
+  const [isLoginActive, setIsLoginActive] = useState(true);
+
+  const switchAuthHandler = () => {
+    setIsLoginActive(prevState => !prevState);
+  };
+
+  return (
+    <AuthContainer>
+      {isLoginActive ? (
+        <Register onSwitch={switchAuthHandler} />
+      ) : (
+        <Login onSwitch={switchAuthHandler} />
+      )}
     </AuthContainer>
   );
 };
