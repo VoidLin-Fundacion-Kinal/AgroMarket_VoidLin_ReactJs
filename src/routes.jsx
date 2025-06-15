c55ef6d5f8c2f6088392d5d578beb832ecbf6c5e
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-14 19:39:55 -0600

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
-import { AuthPages } from './pages/AuthPages/AuthPages.jsx'
+import { AuthPages, AuthPagesRegister } from './pages/AuthPages/AuthPages.jsx'
 import Home from './pages/Home/Home.jsx'
 import { Login } from './components/Auth/Login.jsx'
 import { Register } from './components/Auth/Register.jsx'
+import NotFound from './pages/NotFound/NotFound.jsx'
+import CatalogProducts from './pages/Catalog/CatalogProducts.jsx'
 
 
 
@@ -17,16 +19,26 @@ export const routes = [
 
     {
         path: '/auth/login',
-        element: <Login />
+        element: <AuthPages />
     },
     {
         path: '/auth/register',
-        element: <Register />
+        element: <AuthPagesRegister />
     },
     
     {
         path: '/home',
         element: <Home />
+    },
+
+    {
+        path: '/catalog/products',
+        element: <CatalogProducts />
+    },
+
+    {
+        path: '*',
+        element: <NotFound />
     }
     /*
     {
