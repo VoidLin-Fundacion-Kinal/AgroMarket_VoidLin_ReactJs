4f4968c887e70d4f1903c112b521920a481efe8f
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-14 19:39:43 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
-
-import './App.css'
-import { useRoutes } from 'react-router-dom'
-import { routes } from './routes.jsx'
-import { Toaster } from 'react-hot-toast'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout.jsx';
+import Home from './pages/Home/Home.jsx';
+import CatalogProducts from './pages/Catalog/CatalogProducts.jsx';
+import { AuthPages, AuthPagesRegister } from './pages/AuthPages/AuthPages.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
 
 function App() {
-  
-  const elements = useRoutes(routes)
-
   return (
-    <>
-      {elements}
-      <Toaster position='bottom-center'  reverseOrder={false} />
-    </>
-  )
+    <Routes>
+      {/* Rutas que usan Navbar y Footer */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="catalog/products" element={<CatalogProducts />} />
+      </Route>
+
+      {/* Rutas sin layout (como login/register) */}
+      <Route path="/auth" element={<AuthPages />} />
+      <Route path="/auth/login" element={<AuthPages />} />
+      <Route path="/auth/register" element={<AuthPagesRegister />} />
+
+      {/* Página no encontrada */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+
+  );
 }
 
-export default App
+export default App;
