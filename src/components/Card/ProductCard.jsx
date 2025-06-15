61c1ad2aca73476219f8d40ca7e3d5ca36bf2dfa
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-14 19:39:47 -0600

    Create ProductCard.jsx

---
+++ src/components/Card/ProductCard.jsx
@@ -0,0 +1,16 @@
+export const ProductCard = ({ product, onClick }) => {
+  return (
+    <div
+      onClick={() => onClick(product)}
+      className="bg-white rounded-xl shadow hover:shadow-lg p-4 cursor-pointer transition-all"
+    >
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-full h-48 object-cover rounded-md"
+      />
+      <h3 className="text-lg font-semibold mt-2">{product.name}</h3>
+      <p className="text-green-700 font-bold text-md">${product.price.toFixed(2)}</p>
+    </div>
+  );
+};
