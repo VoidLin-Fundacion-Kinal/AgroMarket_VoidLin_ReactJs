bcf0fd118180b33acb4cb37b6e47607fb9e3206c
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-16 10:03:10 -0600

    Update ProductCard.jsx

--- src/components/Card/ProductCard.jsx
+++ src/components/Card/ProductCard.jsx
@@ -5,12 +5,12 @@ export const ProductCard = ({ product, onClick }) => {
       className="bg-white rounded-xl shadow hover:shadow-lg p-4 cursor-pointer transition-all"
     >
       <img
-        src={product.image}
+        src={`http://localhost:2003/images/productsImages/${product.image}`}
         alt={product.name}
         className="w-full h-48 object-cover rounded-md"
       />
       <h3 className="text-lg font-semibold mt-2">{product.name}</h3>
-      <p className="text-green-700 font-bold text-md">${product.price.toFixed(2)}</p>
+      <p className="text-green-700 font-bold text-md">Q{product.price.toFixed(2)}</p>
     </div>
   );
 };
