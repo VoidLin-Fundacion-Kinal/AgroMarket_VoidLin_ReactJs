1bbea905af3f7a239d811ce05a9f74d5899534d7
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-23 23:30:19 -0600

    Update Layout.jsx

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -8,9 +8,8 @@ const Layout = () => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      {/* Ajuste importante aquí */}
       <main className="flex-grow">
-        <div className="min-h-[calc(100vh-64px-64px)]"> {/* Ajusta 64px al alto de tu Navbar y Footer */}
+        <div className="min-h-[calc(100vh-64px-64px)]"> 
           <Outlet />
         </div>
       </main>
